5a9cf4292a9fd360724a1feb2f7e2d0839197720
Lokeshgpt031 <62788101+Lokeshgpt031@users.noreply.github.com>
2025-01-02 16:45:35 +0530

    Update main.py
    Add info

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import yfinance as yf
 import os
 import json
@@ -14,7 +13,8 @@ def hello_world():
     data = {
         "Get Stock Price": "/api/stockprice/<name>",
         "Get Stock History": "/api/history/<name>/<period>",
-        "Get Earning History": "/api/earninghistory/<name>"
+        "Get Earning History": "/api/earninghistory/<name>",
+        "Get Ticker Info": "/api/info/<name>"
     }
     return render_template('table.html', data=data)
 
@@ -29,18 +29,22 @@ def get_stock_price(name):
             datares[i] = ''  # Set to empty string if key is not found
     return json.dumps(datares)
 
-
 @app.route("/api/history/<name>/<period>")
-def get_stock_history(name,period):
-  ticker=yf.Ticker(name).history(period)
-  return ticker.to_json()
-
+def get_stock_history(name, period):
+    ticker = yf.Ticker(name).history(period)
+    return ticker.to_json()
 
 @app.route("/api/earninghistory/<name>")
 def earning_history(name):
-  ticker=yf.Ticker(name).earnings_history.reset_index()
-  ticker.index.name="Dates"
-  return (ticker.to_json())
+    ticker = yf.Ticker(name).earnings_history.reset_index()
+    ticker.index.name = "Dates"
+    return ticker.to_json()
+
+@app.route("/api/info/<name>")
+def get_ticker_info(name):
+    ticker = yf.Ticker(name)
+    info = ticker.info
+    return json.dumps(info)
 
 if __name__ == "__main__":
-  app.run()
+    app.run()
